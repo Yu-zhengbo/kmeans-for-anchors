8651d0b7c1e7d5939ef2aaf3444118f8da7737ce
Yu-zhengbo <72426381+Yu-zhengbo@users.noreply.github.com>
2021-06-25 15:10:01 +0800

    Update k_means.py

--- k_means.py
+++ k_means.py
@@ -1,21 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 25 14:36:50 2021
+
+@author: 24132
+"""
+
 import os
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-file = r'/home/yuzhengbo/下载/yolov4-pytorch-master/input/ground-truth'
+file = r'D:\浏览器下载\yolov4-pytorch-master_\yolov4-pytorch-master\2007_train.txt'
 
-list_dir = os.listdir(file)
 data = []
-for i in list_dir:
-    with open(file+'/'+i,'r')as f:
-        a = f.readlines()
-        a = [i.split('\n')[0] for i in a]
-        a = [i.split(' ')[1:] for i in a]
-        data.extend(a)
-        
-# print(data)
+with open(file,'r')as f:
+    a = f.readlines()
+    a = [i.split(' ')[1:] for i in a]
+    for i in a:
+        for j in i:
+            data.append(j)
+    data = [i.split(',')[:-1] for i in data]
+print(data)
 
 def fun1(data):
     data_change = []
@@ -25,9 +31,12 @@ def fun1(data):
         data_change.append([w,h])
     return data_change
 
+# print(fun1(data))
+
+
 data = np.array(fun1(data))
 
-k_means = KMeans(n_clusters=9, random_state=10)
+k_means = KMeans(n_clusters=15, random_state=10)
 
 k_means.fit(data)
 
@@ -54,16 +63,32 @@ res = []
 for i in sort[-9:]:
     res.append(values.index(i))
     
-# print(res)
+print(sum_)
+print(res)
     
 anchor = []
 for i in range(len(res)):
-    anchor.append(k_means.cluster_centers_[i])
-print(anchor)
+    anchor.append(k_means.cluster_centers_[res[i]])
+# print(anchor)
 
-# ratio = 2000/1664
 
-# for i in anchor:
-#     print((i/ratio).astype(int))
+ratio = 2000/416
+res = []
+for i in anchor:
+    res.append(list((i/ratio).astype(int)))
+# print(res)
 
+def fun2(res):
+    res_area = [i[0]*i[1] for i in res]
+    area_sort = sorted(res_area)
+    index = []
+    for i in area_sort:
+        index.append(res_area.index(i))
+    res_ = []
+    # print(index)
+    for i in index:
+        res_.append(res[i])
+    print(res_)
+print('\nfinal result:')    
+fun2(res)
 
